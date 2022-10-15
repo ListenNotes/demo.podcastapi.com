958ee0110be7cc75370fa2ac22e0a06bfe64f769
Wenbin Fang <wenbin@listennotes.com>
2022-10-15 12:25:12 -0700

    Use new podcast api for cloudflare workers

--- functions/api/search.js
+++ functions/api/search.js
@@ -1,3 +1,5 @@
+const {Client} = require('podcast-api-cloudflare-workers');
+
 export async function onRequest(context) {
     // Contents of context object
     // const {
@@ -9,33 +11,12 @@ export async function onRequest(context) {
     //   data, // arbitrary space for passing data between middlewares
     // } = context;
 
-    const queryParams = new URLSearchParams({
-        q: 'startup',
+    const client = Client();
+    const term = 'elon musk';
+    const res = await client.search({
+        q: term,
     });
-    const url = `https://listen-api-test.listennotes.com/api/v2/search?${queryParams}`;
-
-    // let formData = new FormData();
-    // formData.append('name', 'John');
-    // formData.append('password', 'John123');
-
-    const config = {
-        method: 'GET',
-        headers: {
-            'X-ListenAPI-Key': null,
-        },
-        // body: formData,
-    };
-    const res = await fetch(url, config);
     const results = await res.json();
-
-    /*
-    X-ListenAPI-FreeQuota: 25000
-    X-ListenAPI-Usage: 19231
-    X-listenAPI-Latency-Seconds: 0.056
-    X-Listenapi-NextBillingDate: 2020-09-26T17:27:33.110641+00:00
-     */
-    console.log(res.headers.get('X-ListenAPI-FreeQuota'));
-
     return new Response(JSON.stringify(results), {
         headers: {
             'content-type': 'application/json;charset=UTF-8',
