d5a54566f51da6217e5b98329abbf8cd32a53f51
Wenbin Fang <wenbin@listennotes.com>
2022-10-14 19:37:59 -0700

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hello, react app!
-        </p>
-      </header>
+    <div className="text-3xl font-bold underline">
+        hello, react app!
     </div>
   );
 }
